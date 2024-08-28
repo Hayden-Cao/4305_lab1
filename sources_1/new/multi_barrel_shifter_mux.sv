a4e8e5ad7e8e837304021710f820c0f26a4987b5
Hayden Cao <130268332+Hayden-Cao@users.noreply.github.com>
2024-08-28 12:38:29 -0700

    Add files via upload

--- sources_1/new/multi_barrel_shifter_mux.sv
+++ sources_1/new/multi_barrel_shifter_mux.sv
@@ -3,18 +3,18 @@
 module multi_barrel_shifter_mux
 #(parameter N = 3)
 (
-    input logic [2**N - 1:0] a,
+    input logic [2**N - 1:0] data_in,
     input logic lr,
     input logic [N-1:0] amount,
-    output logic [2**N - 1:0] y
+    output logic [2**N - 1:0] data_out
 );
 
 logic [((2**N)-1):0] left_out, right_out;
 
-param_left_shifter #(N) left(a,amount,left_out);
-param_right_shifter #(N) right(a,amount,right_out);
+param_left_shifter #(N) left(data_in,amount,left_out);
+param_right_shifter #(N) right(data_in,amount,right_out);
 
-assign y = lr ? left_out : right_out;
+assign data_out = lr ? left_out : right_out;
 
 
 endmodule
